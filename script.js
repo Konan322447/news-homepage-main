6819c3e0344711873e42a4d23cde5e6449c08cd2
Konan322447 <chukwuebukae888@gmail.com>
2024-12-11 17:26:59 +0100

    waka

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const closeMenu = document.getElementById('menuClose');
 
 openMenu.addEventListener('click', () =>{
     navMenu.style.right = '0';
+    navMenu.style.display = 'block';
     overlay.style.display = 'block';
     document.body.style.overflow = 'hidden';
 });
 
 closeMenu.addEventListener('click', () =>{
     navMenu.style.right = '-75dvw';
+    navMenu.style.display = 'none';
     overlay.style.display = 'none';
     document.body.style.overflow = 'auto';
 });
